2970fe1d86cc78d0193478e53827ef5d96e3a013
JH989876525 <ruihonghong1995@gmail.com>
2023-09-23 16:21:32 +0800

    fix: add swapoff to script

--- cleanTrash.sh
+++ cleanTrash.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+echo "cleaning trash"
+
 sudo rm -f ./vivado*.log
 sudo rm -f ./vivado*.jou
 sudo rm -f ./vivado*.str
+sudo rm -f ./vitis*.str
+sudo rm -f ./hs_err*.str
+sudo rm -f ./hs_err*.log
+sudo rm -f ./xrc.log
 sudo rm -rf ./Xil
 sudo rm -rf ~/.local/share/Trash/*
 sudo rm -rf /media/jhh/ExtraSSD/.Trash-1000/*
 sudo rm -rf /media/jhh/2022/.Trash-1000/*
 sudo rm -rf /media/jhh/2023/.Trash-1000/*
 sudo trash-empty
+
+echo "cleaning swap"
+
+sudo swapoff -a
