2dd2fbc2d56668be30ccae0a99ec629f3eee5c14
JH989876525 <ruihonghong1995@gmail.com>
2023-11-30 13:44:20 +0800

    feat: update section function & namespace

--- lib/cpp/timer.hpp
+++ lib/cpp/timer.hpp
@@ -7,21 +7,27 @@
 #define __TIMER__
 
 #include <unistd.h>
-
-class timer{
-    public:
-    void start(){
-        time_start = std::chrono::high_resolution_clock::now();
-    };
-    void end(){
-        time_end = std::chrono::high_resolution_clock::now();
-        duration = time_end - time_start;
-        std::cout << (duration.count() / 1000) << std::endl;
-    };
-    private:
-    std::chrono::high_resolution_clock::time_point time_start;
-    std::chrono::high_resolution_clock::time_point time_end;
-    std::chrono::duration<double, std::milli> duration;
-};
+namespace jhh
+{
+    class timer{
+        public:
+        void start(){
+            time_start = std::chrono::high_resolution_clock::now();
+        };
+        void section(const char* info = ""){
+            end(info);
+            start();
+        };
+        void end(const char* info = ""){
+            time_end = std::chrono::high_resolution_clock::now();
+            duration = time_end - time_start;
+            std::cout << (duration.count() / 1000) << " " << info << std::endl;
+        };
+        private:
+        std::chrono::high_resolution_clock::time_point time_start;
+        std::chrono::high_resolution_clock::time_point time_end;
+        std::chrono::duration<double, std::milli> duration;
+    };  
+} // namespace name
 
 #endif /* __TIMER__ */
